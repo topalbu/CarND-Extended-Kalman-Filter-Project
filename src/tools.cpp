15776ec94a22dc4596131bd2d4be40c513c2752c
topalbu <buraktopal53@gmail.com>
2017-04-25 00:39:40 +0300

    CalculateJacobian method is implemented.

--- src/tools.cpp
+++ src/tools.cpp
@@ -9,11 +9,11 @@ Tools::Tools() {}
 
 Tools::~Tools() {}
 
-//! Calculate the root mean square error
+//! Calculates the root mean square error
 /*!
-   \param estimations estimated state in 4-d vector of type VectorXd
-   \param ground_truth actual state in 4-d vector of type VectorXd
-   \return rmse in 4-d vector of type VectorXd
+   \param estimations estimated state as 4-d vector of type VectorXd
+   \param ground_truth actual state as 4-d vector of type VectorXd
+   \return rmse as 4-d vector of type VectorXd
 */
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
@@ -54,9 +54,34 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     return rmse;
 }
 
+//! Calculates the Jacobian Matrix
+/*!
+   \param x_state robot state as 4-d vector
+   \return Jacobian Maxtix as 3x4 matrix
+*/
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+    MatrixXd Hj(3,4);
+    //recover state parameters
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    //pre-compute a set of terms to avoid repeated calculation
+    float c1 = px*px+py*py;
+    float c2 = sqrt(c1);
+    float c3 = (c1*c2);
+
+    //check division by zero
+    if(fabs(c1) < 0.0001){
+        cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+        return Hj;
+    }
+
+    //compute the Jacobian matrix
+    Hj << (px/c2), (py/c2), 0, 0,
+            -(py/c1), (px/c1), 0, 0,
+            py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+    return Hj;
 }
