c4a8bb87b783a932307436ba70c34b003d9fd539
topalbu <buraktopal53@gmail.com>
2017-06-14 01:06:28 +0300

    clean up.

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/burak/git-ws/kalman_filter/CarND-Extended-Kalman-Filter-Project
-# Build directory: /home/burak/git-ws/kalman_filter/CarND-Extended-Kalman-Filter-Project/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
